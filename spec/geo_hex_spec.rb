f06294e98e62b9dea8d2435b032927a5ecbbf873
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2012-05-30 18:15:06 +0100

    Cleaning up

--- spec/geo_hex_spec.rb
+++ spec/geo_hex_spec.rb
@@ -13,4 +13,19 @@ describe GeoHex do
     end
   end
 
+  it "should decode strings" do
+    described_class.decode("OY").should be_instance_of(GeoHex::Zone)
+  end
+
+  it "should decode correctly" do
+    CSV.foreach(File.expand_path("../cases.csv", __FILE__)) do |lat, lon, level, code|
+      lat, lon, level = lat.to_f, lon.to_f, level.to_i
+      tile = GeoHex::LL.new(lat, lon).to_tile(level)
+
+      zone = described_class.decode(code)
+      [zone.to_s, zone.level, zone.x, zone.y].should == [code, level, tile.x, tile.y]
+      zone.lon.should be_within(180.0).of(0.0)
+    end
+  end
+
 end
