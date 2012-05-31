abe0aeb35986454572fb6bf5d7f710137c921e77
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2012-05-31 15:25:38 +0100

    Fixes polygon

--- lib/geo_hex/polygon.rb
+++ lib/geo_hex/polygon.rb
@@ -3,40 +3,40 @@ module GeoHex
   class Polygon < Struct.new(:easting, :northing, :size)
 
     # @return [GeoHex::PP] The Northeast point of the Zone
-    def north_east_point
-      @north_east_point ||= PP.new(east, north)
+    def north_east
+      @north_east ||= PP.new(east_bound, north_bound)
     end
-    alias_method :ne, :north_east_point
+    alias_method :ne, :north_east
 
     # @return [GeoHex::PP] The East point of the Zone
-    def east_point
-      @east_point ||= PP.new(easting + 2 * size, northing)
+    def east
+      @east ||= PP.new(easting + 2 * size, northing)
     end
-    alias_method :e, :east_point
+    alias_method :e, :east
 
     # @return [GeoHex::PP] The Southeast point of the Zone
-    def south_east_point
-      @south_east_point ||= PP.new(east, south)
+    def south_east
+      @south_east ||= PP.new(east_bound, south_bound)
     end
-    alias_method :se, :south_east_point
+    alias_method :se, :south_east
 
     # @return [GeoHex::PP] The Southwest point of the Zone
-    def south_west_point
-      @south_west_point ||= PP.new(west, south)
+    def south_west
+      @south_west ||= PP.new(west_bound, south_bound)
     end
-    alias_method :sw, :south_west_point
+    alias_method :sw, :south_west
 
     # @return [GeoHex::PP] The West point of the Zone
-    def west_point
-      @west_point ||= PP.new(easting - 2 * size, northing)
+    def west
+      @west ||= PP.new(easting - 2 * size, northing)
     end
-    alias_method :w, :west_point
+    alias_method :w, :west
 
     # @return [GeoHex::PP] The Northwest point of the Zone
-    def north_west_point
-      @north_west_point ||= PP.new(west, north)
+    def north_west
+      @north_west ||= PP.new(west_bound, north_bound)
     end
-    alias_method :nw, :north_west_point
+    alias_method :nw, :north_west
 
     # @return [Array<GeoHex::PP>] All the points of the Zone, ordered from Northeast round to Northwest
     def points
@@ -46,23 +46,23 @@ module GeoHex
     private
 
       # @return [Float] The northing of the Northern boundary of the Zone
-      def north
-        @north ||= northing + H_DEG * size
+      def north_bound
+        @north_bound ||= northing + H_DEG * size
       end
 
       # @return [Float] The easting of both Eastern corners of the Zone
-      def east
-        @east ||= easting + size
+      def east_bound
+        @east_bound ||= easting + size
       end
-      
+
       # @return [Float] The northing of the Southern boundary of the Zone
-      def south
-        @north ||= northing - H_DEG * size
+      def south_bound
+        @south_bound ||= northing - H_DEG * size
       end
 
       # @return [Float] The easting of both Western corners of the Zone
-      def west
-        @west ||= easting - size
+      def west_bound
+        @west_bound  ||= easting - size
       end
 
   end
--- spec/geo_hex/polygon_spec.rb
+++ spec/geo_hex/polygon_spec.rb
@@ -10,51 +10,51 @@ describe GeoHex::Polygon do
     GeoHex::PP.new(*args)
   end
 
-  its(:north_east_point) { should be_a(GeoHex::PP) }
-  its(:north_east_point) { should == projection(-16966.67, 6710505.672772524) }
-  it "should alias north_east_point to ne" do
-    subject.north_east_point.should eq(subject.ne)
+  its(:north_east) { should be_a(GeoHex::PP) }
+  its(:north_east) { should == projection(-16966.67, 6710505.672772524) }
+  it "should alias north_east to ne" do
+    subject.north_east.should eq(subject.ne)
   end
 
-  its(:east_point) { should be_a(GeoHex::PP) }
-  its(:east_point) { should == projection(-16627.34, 6710328) }
-  it "should alias east_point to e" do 
-    subject.east_point.should eq(subject.e)
+  its(:east) { should be_a(GeoHex::PP) }
+  its(:east) { should == projection(-16627.34, 6710328) }
+  it "should alias east to e" do
+    subject.east.should eq(subject.e)
   end
 
-  its(:south_east_point) { should be_a(GeoHex::PP) }
-  its(:south_east_point) { should == projection(-16966.67, 6710150.327227476 ) }
-  it "should alias south_east_point to se" do 
-    subject.south_east_point.should eq(subject.se)
+  its(:south_east) { should be_a(GeoHex::PP) }
+  its(:south_east) { should == projection(-16966.67, 6710150.327227476 ) }
+  it "should alias south_east to se" do
+    subject.south_east.should eq(subject.se)
   end
 
-  its(:south_west_point) { should be_a(GeoHex::PP) }
-  its(:south_west_point) { should == projection(-17645.33, 6710150.327227476) }
-  it "should alias south_west_point to sw" do 
-    subject.south_west_point.should eq(subject.sw)
+  its(:south_west) { should be_a(GeoHex::PP) }
+  its(:south_west) { should == projection(-17645.33, 6710150.327227476) }
+  it "should alias south_west to sw" do
+    subject.south_west.should eq(subject.sw)
   end
 
-  its(:west_point) { should be_a(GeoHex::PP) }
-  its(:west_point) { should == projection(-17984.66, 6710328) }
-  it "should alias west_point to w" do 
-    subject.west_point.should eq(subject.w)
+  its(:west) { should be_a(GeoHex::PP) }
+  its(:west) { should == projection(-17984.66, 6710328) }
+  it "should alias west to w" do
+    subject.west.should eq(subject.w)
   end
 
-  its(:north_west_point) { should be_a(GeoHex::PP) }
-  its(:north_west_point) { should == projection(-17645.33, 6710505.672772524) }
-  it "should alias north_west_point to w" do 
-    subject.north_west_point.should eq(subject.nw)
+  its(:north_west) { should be_a(GeoHex::PP) }
+  its(:north_west) { should == projection(-17645.33, 6710505.672772524) }
+  it "should alias north_west to w" do
+    subject.north_west.should eq(subject.nw)
   end
 
   describe "points" do
     it "should include all 6 coordinates from NE round to NW" do
       subject.points.should == [
-        subject.north_east_point,
-        subject.east_point,
-        subject.south_east_point,
-        subject.south_west_point,
-        subject.west_point,
-        subject.north_west_point
+        subject.north_east,
+        subject.east,
+        subject.south_east,
+        subject.south_west,
+        subject.west,
+        subject.north_west
       ]
     end
   end
