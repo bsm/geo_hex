81d4e7f4697ba92c967881350975711e19c8ec47
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2012-05-31 13:19:01 +0100

    Added new benchmark

---
+++ benchmark/neighbours.rb
@@ -0,0 +1,27 @@
+require File.expand_path("../helper", __FILE__)
+
+GeoHex::BM.new(100) do
+  zone = GeoHex::Zone.new(5700, 5717, 7)
+
+  report "Range: 1" do
+    zone.neighbours(1)
+  end
+
+  report "Range: 2" do
+    zone.neighbours(2)
+  end
+
+  report "Range: 3" do
+    zone.neighbours(3)
+  end
+
+  report "Range: 20" do
+    zone.neighbours(20)
+  end
+
+  report "Range: 100" do
+    zone.neighbours(100)
+  end
+
+end
+
