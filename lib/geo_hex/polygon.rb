aab239e8b3f716f6e96444ac40e892d6a0fdfc06
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2012-06-01 07:37:03 +0100

    Improved polygon accuracies

--- lib/geo_hex/polygon.rb
+++ lib/geo_hex/polygon.rb
@@ -3,43 +3,49 @@ module GeoHex
   class Polygon < Struct.new(:easting, :northing, :size)
     H_K = Math.tan(Math::PI / 180.0 * 60)
 
-    # @return [GeoHex::PP] The Northeast point of the Zone
+    # @return [GeoHex::PP] The Centroid of the Polygon
+    def centroid
+      @centroid ||= PP.new(easting, northing)
+    end
+    alias_method :c, :centroid
+
+    # @return [GeoHex::PP] The Northeast point of the Polygon
     def north_east
       @north_east ||= PP.new(east_bound, north_bound)
     end
     alias_method :ne, :north_east
 
-    # @return [GeoHex::PP] The East point of the Zone
+    # @return [GeoHex::PP] The East point of the Polygon
     def east
       @east ||= PP.new(easting + 2 * size, northing)
     end
     alias_method :e, :east
 
-    # @return [GeoHex::PP] The Southeast point of the Zone
+    # @return [GeoHex::PP] The Southeast point of the Polygon
     def south_east
       @south_east ||= PP.new(east_bound, south_bound)
     end
     alias_method :se, :south_east
 
-    # @return [GeoHex::PP] The Southwest point of the Zone
+    # @return [GeoHex::PP] The Southwest point of the Polygon
     def south_west
       @south_west ||= PP.new(west_bound, south_bound)
     end
     alias_method :sw, :south_west
 
-    # @return [GeoHex::PP] The West point of the Zone
+    # @return [GeoHex::PP] The West point of the Polygon
     def west
       @west ||= PP.new(easting - 2 * size, northing)
     end
     alias_method :w, :west
 
-    # @return [GeoHex::PP] The Northwest point of the Zone
+    # @return [GeoHex::PP] The Northwest point of the Polygon
     def north_west
       @north_west ||= PP.new(west_bound, north_bound)
     end
     alias_method :nw, :north_west
 
-    # @return [Array<GeoHex::PP>] All the points of the Zone, ordered from Northeast round to Northwest
+    # @return [Array<GeoHex::PP>] All the points of the Polygon, ordered from Northeast round to Northwest
     def points
       @points ||= [ne, e, se, sw, w, nw]
     end
@@ -47,22 +53,22 @@ module GeoHex
 
     private
 
-      # @return [Float] The northing of the Northern boundary of the Zone
+      # @return [Float] The northing of the Northern boundary of the Polygon
       def north_bound
         @north_bound ||= northing + H_K * size
       end
 
-      # @return [Float] The easting of both Eastern corners of the Zone
+      # @return [Float] The easting of both Eastern corners of the Polygon
       def east_bound
         @east_bound ||= easting + size
       end
 
-      # @return [Float] The northing of the Southern boundary of the Zone
+      # @return [Float] The northing of the Southern boundary of the Polygon
       def south_bound
         @south_bound ||= northing - H_K * size
       end
 
-      # @return [Float] The easting of both Western corners of the Zone
+      # @return [Float] The easting of both Western corners of the Polygon
       def west_bound
         @west_bound  ||= easting - size
       end
--- spec/geo_hex/polygon_spec.rb
+++ spec/geo_hex/polygon_spec.rb
@@ -5,11 +5,21 @@ describe GeoHex::Polygon do
   let(:pg) { GeoHex.decode("QE0166383").polygon }
   subject  { pg }
 
+  describe "centroid" do
+    subject { pg.centroid.to_ll }
+
+    its(:lat) { should be_within(10**-6).of(51.500613) }
+    its(:lon) { should be_within(10**-6).of(-0.155464) }
+    it "should alias to c" do
+      pg.c.should eq(pg.centroid)
+    end
+  end
+
   describe "east" do
     subject { pg.east.to_ll }
 
-    its(:lat) { should be_within(0.001).of(51.500) }
-    its(:lon) { should be_within(0.001).of(-0.149) }
+    its(:lat) { should be_within(10**-6).of(51.500613) }
+    its(:lon) { should be_within(10**-6).of(-0.149367) }
     it "should alias to e" do
       pg.e.should eq(pg.east)
     end
@@ -18,8 +28,8 @@ describe GeoHex::Polygon do
   describe "west" do
     subject { pg.west.to_ll }
 
-    its(:lat) { should be_within(0.001).of(51.500) }
-    its(:lon) { should be_within(0.001).of(-0.161) }
+    its(:lat) { should be_within(10**-6).of(51.500613) }
+    its(:lon) { should be_within(10**-6).of(-0.161560) }
     it "should alias to w" do
       pg.w.should eq(pg.west)
     end
@@ -28,8 +38,8 @@ describe GeoHex::Polygon do
   describe "north east" do
     subject { pg.north_east.to_ll }
 
-    its(:lat) { should be_within(0.001).of(51.503) }
-    its(:lon) { should be_within(0.001).of(-0.152) }
+    its(:lat) { should be_within(10**-6).of(51.503899) }
+    its(:lon) { should be_within(10**-6).of(-0.152415) }
     it "should alias to ne" do
       pg.ne.should eq(pg.north_east)
     end
@@ -38,8 +48,8 @@ describe GeoHex::Polygon do
   describe "north west" do
     subject { pg.north_west.to_ll }
 
-    its(:lat) { should be_within(0.001).of(51.503) }
-    its(:lon) { should be_within(0.001).of(-0.158) }
+    its(:lat) { should be_within(10**-6).of(51.503899) }
+    its(:lon) { should be_within(10**-6).of(-0.158512) }
     it "should alias to nw" do
       pg.nw.should eq(pg.north_west)
     end
@@ -48,8 +58,8 @@ describe GeoHex::Polygon do
   describe "south east" do
     subject { pg.south_east.to_ll }
 
-    its(:lat) { should be_within(0.001).of(51.497) }
-    its(:lon) { should be_within(0.001).of(-0.152) }
+    its(:lat) { should be_within(10**-6).of(51.497326) }
+    its(:lon) { should be_within(10**-6).of(-0.152415) }
     it "should alias to se" do
       pg.se.should eq(pg.south_east)
     end
@@ -58,8 +68,8 @@ describe GeoHex::Polygon do
   describe "south west" do
     subject { pg.south_west.to_ll }
 
-    its(:lat) { should be_within(0.001).of(51.497) }
-    its(:lon) { should be_within(0.001).of(-0.158) }
+    its(:lat) { should be_within(10**-6).of(51.497326) }
+    its(:lon) { should be_within(10**-6).of(-0.158512) }
     it "should alias to sw" do
       pg.sw.should eq(pg.south_west)
     end
